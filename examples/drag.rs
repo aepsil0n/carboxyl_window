e82b626cfb0a253e28414697b07569bbbfe91b0a
Eduard Bopp <eduard.bopp@aepsil0n.de>
2015-02-19 17:51:23 +0100

    Add a mouse drag & drop example

--- examples/drag.rs
+++ examples/drag.rs
@@ -1,5 +1,3 @@
-#![feature(core)]
-
 extern crate graphics;
 extern crate glium;
 extern crate glutin;
@@ -13,10 +11,10 @@ extern crate carboxyl_window;
 
 fn main() {
     use glium::{DisplayBuild, Surface};
-    use glium_graphics::GliumSurfaceBackEnd;
+    use glium_graphics::{GliumBackendSystem, GliumSurfaceBackEnd};
     use graphics::Context;
     use input::{Button, Key, MouseButton};
-    //use carboxyl::{Cell, Sink};
+    use carboxyl::CellCycle;
     use carboxyl_window::{GliumWindow, Window, ButtonState, ButtonEvent};
 
     let display = glutin::WindowBuilder::new()
@@ -28,58 +26,74 @@ fn main() {
     let buttons = window.buttons();
     let cursor = window.cursor();
 
-    let spawn = cursor.snapshot(
-        &buttons.map(|button| match button {
+    let rects = CellCycle::<Vec<Rect>>::new(vec![Rect(100, 100)]);
+
+    let events = lift!(|c, r| (c, r), &cursor, &rects)
+        .snapshot(&buttons)
+        .filter_map(|((pos, rects), button)| match button {
             ButtonEvent {
                 button: Button::Keyboard(Key::Space),
                 state: ButtonState::Pressed,
-            } => Some(()),
-            _ => None,
-        }).filter()
-    ).map(|(pos, _)| Rect(pos.0, pos.1));
+            }
+                => Some(RectEvent::Spawn(Rect(pos.0, pos.1))),
+            ButtonEvent { button: Button::Mouse(MouseButton::Left), state: ButtonState::Pressed }
+                => rects.iter()
+                    .enumerate()
+                    .filter_map(|(k, r)|
+                        if r.contains(pos) { Some(RectEvent::Drag(k, pos)) }
+                        else { None })
+                    .next(),
+            ButtonEvent { button: Button::Mouse(MouseButton::Left), state: ButtonState::Released }
+                => Some(RectEvent::Drop),
+            _   => None,
+        });
 
-    let clicks = buttons.map(|button| match button {
-        ButtonEvent { button: Button::Mouse(MouseButton::Left), state }
-          => Some(state),
-        _ => None,
-    }).filter();
-
-    let spawned_rects = spawn.accumulate(vec![],
-        |(mut last, new)| { last.push(new); last });
-
-    let drag = lift!(|a, b| (a, b), &spawned_rects, &cursor)
-        .snapshot(&clicks)
-        .map(|((rects, pos), state)| match state {
-            ButtonState::Pressed => rects.iter()
-                .enumerate()
-                .filter_map(|(k, r)| if r.contains(pos) { Some(k) } else { None })
-                .next(),
-            ButtonState::Released => None,
+    let spawned = rects.snapshot(&events)
+        .map(|(mut rects, ev)| match ev {
+            RectEvent::Spawn(r) => { rects.push(r); rects },
+            _ => rects,
         })
-        .hold(None);
+        .hold(vec![Rect(300, 500)]);
 
-    let drag_rect = lift!(|rects, idx| match idx {
-        Some(idx) => Some(Rect(rects[idx].0 + 200, rects[idx].1)),
-        None => None,
-    }, &spawned_rects, &drag);
+    let new_rects = events.filter_map({
+        let spawned = spawned.clone();
+        move |ev| match ev {
+            RectEvent::Drag(idx, pos) => {
+            Some(lift!(
+                move |mut rects, mouse| {
+                    rects[idx] = Rect(
+                        rects[idx].0 + (mouse.0 - pos.0),
+                        rects[idx].1 + (mouse.1 - pos.1)
+                    );
+                    rects
+                },
+                &spawned, &cursor
+            ))},
+            RectEvent::Drop => Some(spawned.clone()),
+            _ => None,
+        }})
+        .hold(spawned.clone())
+        .switch();
+    let rects = rects.define(new_rects);
 
-    let _render = lift!(|s, r, d| (s, r, d), &window.size(), &spawned_rects, &drag_rect)
+    let _render = lift!(|s, r| (s, r), &window.size(), &rects)
         .snapshot(&window.ticks())
-        .map(move |(((w, h), rects, drag), _dt)| {
+        .map(move |(((w, h), rects), _dt)| {
             let mut target = display.draw();
             {
-                let mut backend = GliumSurfaceBackEnd::new(display.clone(), &mut target);
+                let mut backend_sys = GliumBackendSystem::new(&display);
+                let mut backend = GliumSurfaceBackEnd::new(&mut backend_sys, &mut target);
                 let context = Context::abs(w as f64, h as f64);
                 graphics::clear([1.0; 4], &mut backend);
-                if let Some(drag) = drag {
-                    graphics::Rectangle::new([0.0, 0.8, 0.0, 0.6])
+                /*if let Some((_, drag)) = drag {
+                    graphics::Rectangle::new([0.0, 0.8, 0.0, 0.7])
                     .draw(
                         [(drag.0 - 50) as f64, (drag.1 - 50) as f64, 100.0, 100.0],
                         &context, &mut backend
                     );
-                }
+                }*/
                 for rect in rects {
-                    graphics::Rectangle::new([1.0, 0.5, 0.0, 0.6])
+                    graphics::Rectangle::new([1.0, 0.3, 0.0, 0.7])
                     .draw(
                         [(rect.0 - 50) as f64, (rect.1 - 50) as f64, 100.0, 100.0],
                         &context, &mut backend
@@ -91,6 +105,14 @@ fn main() {
 
     window.start();
 }
+
+#[derive(Clone, Debug)]
+enum RectEvent {
+    Spawn(Rect),
+    Drag(usize, (i32, i32)),
+    Drop,
+}
+
 
 #[derive(Clone, Debug)]
 struct Rect(i32, i32);
