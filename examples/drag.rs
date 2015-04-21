04a38483056a01545753546cec35c2c844253206
Eduard Bopp <eduard.bopp@aepsil0n.de>
2015-04-21 16:03:31 +0200

    Update example

--- examples/drag.rs
+++ examples/drag.rs
@@ -2,18 +2,25 @@ extern crate graphics;
 extern crate glium;
 extern crate glutin;
 extern crate glium_graphics;
+extern crate shader_version;
 extern crate image;
 extern crate input;
+extern crate window;
+extern crate glutin_window;
 #[macro_use(lift)]
 extern crate carboxyl;
 extern crate carboxyl_window;
 
 
 use glium::{ DisplayBuild, Surface };
-use glium_graphics::{ Glium2d, GliumGraphics, OpenGL };
+use glium_graphics::{ Glium2d, GliumGraphics, GliumWindow };
+use glutin_window::{ GlutinWindow };
+use window::WindowSettings;
 use input::{ Button, Key, MouseButton };
 use carboxyl::{ CellCycle, Cell };
-use carboxyl_window::{ GliumWindow, Window, ButtonState, ButtonEvent };
+use carboxyl_window::{ StreamingWindow, WindowWrapper, ButtonState,
+                       ButtonEvent };
+use shader_version::OpenGL;
 
 
 #[derive(Clone, Debug)]
@@ -37,7 +44,7 @@ impl Rect {
 }
 
 
-fn app_logic(window: &GliumWindow) -> Cell<Vec<Rect>> {
+fn app_logic<W: StreamingWindow>(window: &W) -> Cell<Vec<Rect>> {
     let buttons = window.buttons();
     let cursor = window.cursor();
 
@@ -50,13 +57,15 @@ fn app_logic(window: &GliumWindow) -> Cell<Vec<Rect>> {
                 button: Button::Keyboard(Key::Space),
                 state: ButtonState::Pressed,
             }
-                => Some(RectEvent::Spawn(Rect(pos.0, pos.1))),
+                => Some(RectEvent::Spawn(Rect(pos.0 as i32, pos.1 as i32))),
             ButtonEvent { button: Button::Mouse(MouseButton::Left), state: ButtonState::Pressed }
                 => rects.iter()
                     .enumerate()
-                    .filter_map(|(k, r)|
+                    .filter_map(|(k, r)| {
+                        let pos = (pos.0 as i32, pos.1 as i32);
                         if r.contains(pos) { Some(RectEvent::Drag(k, pos)) }
-                        else { None })
+                        else { None }
+                    })
                     .next(),
             ButtonEvent { button: Button::Mouse(MouseButton::Left), state: ButtonState::Released }
                 => Some(RectEvent::Drop),
@@ -77,8 +86,8 @@ fn app_logic(window: &GliumWindow) -> Cell<Vec<Rect>> {
             Some(lift!(
                 move |mut rects, mouse| {
                     rects[idx] = Rect(
-                        rects[idx].0 + (mouse.0 - pos.0),
-                        rects[idx].1 + (mouse.1 - pos.1)
+                        rects[idx].0 + (mouse.0 as i32 - pos.0),
+                        rects[idx].1 + (mouse.1 as i32 - pos.1)
                     );
                     rects
                 },
@@ -94,22 +103,25 @@ fn app_logic(window: &GliumWindow) -> Cell<Vec<Rect>> {
 
 
 fn main() {
-    let display = glutin::WindowBuilder::new()
-        .with_dimensions(300, 300)
-        .with_title(format!("Image test"))
-        .build_glium().unwrap();
+    use std::rc::Rc;
+    use std::cell::RefCell;
 
-    let window = GliumWindow::new(display, 60);
+    let glutin_window = Rc::new(RefCell::new(GlutinWindow::new(
+        OpenGL::_3_2,
+        WindowSettings::new("Title", (1920, 1080))
+    )));
+    let window = WindowWrapper::new(glutin_window.clone(), 60);
     let rects = app_logic(&window);
     let scene = lift!(|s, r| (s, r), &window.size(), &rects);
+    let glium_window = GliumWindow::new(&glutin_window).unwrap();
+    let mut backend_sys = Glium2d::new(OpenGL::_3_2, &glium_window);
 
-    window.run(|display| {
+    window.run(|| {
         let ((w, h), rects) = scene.sample();
-        let mut target = display.draw();
+        let mut target = glium_window.draw();
         {
-            let mut backend_sys = Glium2d::new(OpenGL::_3_2, &display);
             let mut backend = GliumGraphics::new(&mut backend_sys, &mut target);
-            let transform = graphics::abs_transform(w as f64, h as f64);
+            let transform = graphics::math::abs_transform(w as f64, h as f64);
             graphics::clear([1.0; 4], &mut backend);
             for rect in rects {
                 graphics::Rectangle::new([1.0, 0.3, 0.0, 0.7]).draw(
