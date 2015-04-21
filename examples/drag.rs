cbc9115898c37f6a59531fb541ebb5f5593ae317
Eduard Bopp <eduard.bopp@aepsil0n.de>
2015-04-21 19:13:08 +0200

    Use elmesque for drawing in the example

--- examples/drag.rs
+++ examples/drag.rs
@@ -1,3 +1,4 @@
+extern crate elmesque;
 extern crate graphics;
 extern crate glium;
 extern crate glutin;
@@ -12,15 +13,16 @@ extern crate carboxyl;
 extern crate carboxyl_window;
 
 
-use glium::{ DisplayBuild, Surface };
-use glium_graphics::{ Glium2d, GliumGraphics, GliumWindow };
-use glutin_window::{ GlutinWindow };
+use glium::Surface;
+use glium_graphics::{ Glium2d, GliumGraphics, GliumWindow, GlyphCache };
+use glutin_window::GlutinWindow;
 use window::WindowSettings;
 use input::{ Button, Key, MouseButton };
 use carboxyl::{ CellCycle, Cell };
 use carboxyl_window::{ StreamingWindow, WindowWrapper, ButtonState,
                        ButtonEvent };
 use shader_version::OpenGL;
+use elmesque::{ Form, Renderer };
 
 
 #[derive(Clone, Debug)]
@@ -77,7 +79,7 @@ fn app_logic<W: StreamingWindow>(window: &W) -> Cell<Vec<Rect>> {
             RectEvent::Spawn(r) => { rects.push(r); rects },
             _ => rects,
         })
-        .hold(vec![Rect(300, 500)]);
+        .hold(vec![Rect(0, 0)]);
 
     let new_rects = events.filter_map({
         let spawned = spawned.clone();
@@ -101,36 +103,53 @@ fn app_logic<W: StreamingWindow>(window: &W) -> Cell<Vec<Rect>> {
     rects.define(new_rects)
 }
 
+fn view((width, height): (u32, u32), rects: &Vec<Rect>) -> Form {
+    use elmesque::color::rgba;
+    use elmesque::form::{ group, rect };
+    group(
+        rects.iter()
+        .map(|&Rect(x, y)|
+            rect(100.0, 100.0)
+            .filled(rgba(1.0, 0.3, 0.0, 0.7))
+            .shift(
+                -(width as f64 / 2.0) + x as f64,
+                height as f64 / 2.0 - y as f64
+            )
+        )
+        .collect()
+    )
+}
+
 
 fn main() {
     use std::rc::Rc;
     use std::cell::RefCell;
+    use std::path::Path;
 
     let glutin_window = Rc::new(RefCell::new(GlutinWindow::new(
         OpenGL::_3_2,
         WindowSettings::new("Title", (1920, 1080))
     )));
     let window = WindowWrapper::new(glutin_window.clone(), 60);
-    let rects = app_logic(&window);
-    let scene = lift!(|s, r| (s, r), &window.size(), &rects);
+    let model = app_logic(&window);
+    let scene = lift!(|s, r| (s, view(s, &r)), &window.size(), &model);
     let glium_window = GliumWindow::new(&glutin_window).unwrap();
     let mut backend_sys = Glium2d::new(OpenGL::_3_2, &glium_window);
+    let mut glyph_cache = GlyphCache::new(
+        &Path::new("./assets/NotoSans/NotoSans-Regular.ttf"),
+        glium_window.clone()
+    ).unwrap();
 
     window.run(|| {
-        let ((w, h), rects) = scene.sample();
+        let ((w, h), form) = scene.sample();
         let mut target = glium_window.draw();
         {
             let mut backend = GliumGraphics::new(&mut backend_sys, &mut target);
-            let transform = graphics::math::abs_transform(w as f64, h as f64);
             graphics::clear([1.0; 4], &mut backend);
-            for rect in rects {
-                graphics::Rectangle::new([1.0, 0.3, 0.0, 0.7]).draw(
-                    [(rect.0 - 50) as f64, (rect.1 - 50) as f64, 100.0, 100.0],
-                    graphics::default_draw_state(),
-                    transform,
-                    &mut backend
-                )
-            }
+            let mut renderer = Renderer::new(w as f64, h as f64, &mut backend)
+                .character_cache(&mut glyph_cache);
+            elmesque::form::collage(w as i32, h as i32, vec![form])
+                .draw(&mut renderer);
         }
         target.finish();
     });
