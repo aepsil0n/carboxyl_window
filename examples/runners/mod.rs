baff55b8b573ece9f8a7355a619c4abc17dcd737
Andre-Patrick Bubel <code@andre-bubel.de>
2016-01-13 11:38:33 +0000

    Bump dependency versions and fix compile errors.

--- examples/runners/mod.rs
+++ examples/runners/mod.rs
@@ -17,12 +17,12 @@ pub fn run_glutin<F>(settings: WindowSettings, app: F)
     use std::cell::RefCell;
     use std::path::Path;
 
-    const GLVERSION: OpenGL = OpenGL::_2_1;
+    const GLVERSION: OpenGL = OpenGL::V2_1;
     let glutin_window = Rc::new(RefCell::new(
-        GlutinWindow::new(GLVERSION, settings)));
+        GlutinWindow::new(settings).ok().unwrap()));
     let mut window = SourceWindow::new(glutin_window.clone());
     let scene = lift!(|s, m| (s, m), &window.size(), &app(&window));
-    let glium_window = GliumWindow::new(&glutin_window).unwrap();
+    let glium_window = GliumWindow::new(&glutin_window).ok().unwrap();
     let mut backend_sys = Glium2d::new(GLVERSION, &glium_window);
     let mut glyph_cache = GlyphCache::new(
         &Path::new("./assets/NotoSans/NotoSans-Regular.ttf"),
@@ -34,7 +34,7 @@ pub fn run_glutin<F>(settings: WindowSettings, app: F)
         let mut target = glium_window.draw();
         {
             let mut backend = GliumGraphics::new(&mut backend_sys, &mut target);
-            let mut renderer = Renderer::new(Context::abs(w as f64, h as f64), &mut backend)
+            let mut renderer = Renderer::new(Context::new_abs(w as f64, h as f64), &mut backend)
                 .character_cache(&mut glyph_cache);
             element.draw(&mut renderer);
         }
